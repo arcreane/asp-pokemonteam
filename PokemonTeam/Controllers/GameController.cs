c14131fb668c8816fe23a294fd46aa32bbf0adbe
Squelele <141148037+Squelele@users.noreply.github.com>
2025-07-02 13:44:09 +0200

    ajout d'un game controller

---
+++ PokemonTeam/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// This controller manages the Suika-like Pokemon game.
+/// </summary>
+/// <remarks>
+/// Handles game logic, initial state, and view rendering.
+/// </remarks>
+/// <author>
+/// Elerig
+/// </author>
+public class GameController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
