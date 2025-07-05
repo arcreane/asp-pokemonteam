373b5568dae875b2b364292e1f162f3ab56020fc
Septsept <benyamina.mo.pro@gmail.com>
2025-07-05 22:25:30 +0200

    Feat : Need to be connected to play pokeGacha

--- PokemonTeam/Controllers/PokeGachaController.cs
+++ PokemonTeam/Controllers/PokeGachaController.cs
@@ -1,7 +1,9 @@
 namespace PokemonTeam.Controllers;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+[Authorize]
 public class PokeGachaController : Controller
 {
     public IActionResult Index()
