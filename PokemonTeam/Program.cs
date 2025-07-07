ad4253388d525252fd1f0bdbb7303f2351c28494
Squelele <141148037+Squelele@users.noreply.github.com>
2025-07-07 15:37:19 +0200

    modification nugets

--- PokemonTeam/Program.cs
+++ PokemonTeam/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 // Add secrets to the configuration.
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
